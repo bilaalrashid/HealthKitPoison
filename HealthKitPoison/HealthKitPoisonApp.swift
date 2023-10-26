0845703f0e5fc85bccfad6a6cb60bbc6b313ffb0
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-10-26 22:26:24 +0100

    Initial Commit

---
+++ HealthKitPoison/HealthKitPoisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitPoisonApp.swift
+//  HealthKitPoison
+//
+//  Created by Bilaal Rashid on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitPoisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
